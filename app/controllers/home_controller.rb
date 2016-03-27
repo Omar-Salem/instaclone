dad1f8642ecbd439bc855eade295ef8dd150ac0b
Omar-Salem <nicromonicon@hotmail.com>
2016-03-27 15:06:04 +1000

    getting user's images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
     string = "http://127.0.0.1:3000"
     host=string+"/users/callback"
     client_id="ddc5629e25284f14a02604f268ae09c9"
-    @url="https://instagram.com/oauth/authorize/?client_id=#{client_id}&redirect_uri=#{host}&response_type=token"
+    @url="https://instagram.com/oauth/authorize/?client_id=#{client_id}&redirect_uri=#{host}&response_type=token&scope=public_content"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,21 @@ class UsersController < ApplicationController
     token = params["token"]
     url = "https://api.instagram.com/v1/users/self/media/recent/?access_token=#{token}"
     response = HTTParty.get(url, :verify => false)
-    puts response
+    response["data"].each do |item|
+      next if item["type"]!="image"
+      userId="omar salem"
+      save_img(item["id"], item["images"], userId)
+    end
     render json: response["data"]
 
+  end
+
+  def save_img(imgId, imgDictionary, userId)
+    puts "imgId is "+imgId
+
+    puts imgDictionary["low_resolution"]["url"]
+    puts imgDictionary["thumbnail"]["url"]
+    puts imgDictionary["standard_resolution"]["url"]
   end
 
   def get
