f2ff4195996e57ea4a90e640dfc60786231f5f00
Omar Salem <omar.salem@guvera.com>
2016-03-30 20:03:14 +1000

    added landing page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( app.js )
-Rails.application.config.assets.precompile += %w( controllers/controllersModule.js )
-Rails.application.config.assets.precompile += %w( controllers/callbackController.js )
-Rails.application.config.assets.precompile += %w( controllers/imagesController.js )
+Rails.application.config.assets.precompile += %w( *.js )
+Rails.application.config.assets.precompile += %w( *.css )
+Rails.application.config.assets.precompile += %w( *.jpg )
+# Rails.application.config.assets.precompile += %w( controllers/controllersModule.js )
+# Rails.application.config.assets.precompile += %w( controllers/callbackController.js )
+# Rails.application.config.assets.precompile += %w( controllers/imagesController.js )
